4767634f79e1a11b7184c58eae826eb4f7e6428c
ALUMNO09-J1401 <alumno09-j1401@TLSCPU1992.local>
2019-11-06 20:15:07 -0500

    Initial Commit

---
+++ Review/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Review
+//
+//  Created by ALUMNO09-J1401 on 11/6/19.
+//  Copyright © 2019 Luis Huamani. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Review/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Review
+//
+//  Created by ALUMNO09-J1401 on 11/6/19.
+//  Copyright © 2019 Luis Huamani. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
